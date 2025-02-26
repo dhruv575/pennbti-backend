46f74eec2bfad2c5b58d2ad4144cc581a574a4eb
dhruv575 <dhruv575@gmail.com>
2025-02-26 13:11:09 -0500

    loosened CORS slightly

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const corsOptions = {
   origin: ['http://localhost:3000', 'https://www.pennbti.com'],
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization']
+  allowedHeaders: ['Content-Type', 'Authorization', 'code', 'password']
 };
 
 // Middleware
